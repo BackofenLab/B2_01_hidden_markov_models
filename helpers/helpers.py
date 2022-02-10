66d45b5b36fd1dcfe5381f7ea4dc5e0224f9a34f
Alexander-Mitrofanov <mitrofan@informatik.uni-freiburg.de>
2022-02-10 02:46:37 +0100

    all of the functions implemented

--- helpers/helpers.py
+++ helpers/helpers.py
@@ -40,21 +40,46 @@ def roll_proba_given_dice_correct(list_dice, roll_value):
     return roll_proba
 
 
-def outcome_given_die_correct(die, outcome):
-    outcome_proba = 1
-    for roll in outcome:
-        outcome_proba *= die[str(roll)]
-    return outcome_proba
+def observation_given_die_correct(die, observation):
+    observation_proba = 1
+    for roll in observation:
+        observation_proba *= die[str(roll)]
+    return observation_proba
 
 
-def proba_of_dice_given_outcome(list_dice, outcome):
-    list_outcomes_proba = [outcome_given_die_correct(pair[0], outcome) for pair in list_dice]
-    denominator = sum([op * pair[1] for op, pair in zip(list_outcomes_proba, list_dice)])
+def proba_of_dice_given_observation_correct(list_dice, observation):
+    list_observations_proba = [observation_given_die_correct(pair[0], observation) for pair in list_dice]
+    denominator = sum([op * pair[1] for op, pair in zip(list_observations_proba, list_dice)])
     if denominator == 0:
         return [0] * len(list_dice)
     else:
-        list_dice_proba = [(op * pair[1]) / denominator for op, pair in zip(list_outcomes_proba, list_dice)]
+        list_dice_proba = [(op * pair[1]) / denominator for op, pair in zip(list_observations_proba, list_dice)]
         return list_dice_proba
+
+
+def path_probability_computation_correct(list_dice, path, transition_matrix):
+    path_probability = list_dice[int(path[0])][1]
+    for node1, node2 in zip(path, path[1:]):
+        node1, node2 = int(node1), int(node2)
+        trans_proba = transition_matrix[node1][node2]
+        path_probability *= trans_proba
+    return path_probability
+
+
+def observation_probability_computation_given_path_correct(list_dice, observation, path):
+    observation_probability = 1
+    for roll, node in zip(observation, path):
+        die = list_dice[int(node)][0]
+        roll_proba = die[str(roll)]
+        observation_probability *= roll_proba
+
+    return observation_probability
+
+
+def observation_path_joint_probability_computation_correct(list_dice, observation, path):
+    path_proba = path_probability_computation_correct(list_dice, observation, path)
+    obserbation_proba = observation_probability_computation_given_path_correct(list_dice, observation, path)
+    return path_proba * obserbation_proba
 
 
 def main():
@@ -63,14 +88,24 @@ def main():
     print(die.fair)
     print(die["1"])
     print(die[0])
-    print(outcome_given_die_correct(die, [1, 1, 1, 2]))
+    print(observation_given_die_correct(die, [1, 1, 1, 2]))
 
     fair_die = Die(1/6, 1/6, 1/6, 1/6, 1/6, 1/6)
     loaded_die = Die(0.1, 0.1, 0.1, 0.1, 0.1, 0.5)
     casino_dice = [(fair_die, 0.98), (loaded_die, 0.02)]
     print(roll_proba_given_dice_correct(casino_dice, 6))
-    print(proba_of_dice_given_outcome(casino_dice, [6, 6, 6]))
-    print(proba_of_dice_given_outcome(casino_dice, [6, 6, 6, 6, 6, 6]))
+    print(proba_of_dice_given_observation_correct(casino_dice, [6, 6, 6]))
+    print(proba_of_dice_given_observation_correct(casino_dice, [6, 6, 6, 6, 6, 6]))
+
+    casino_dice = [(fair_die, 0.9), (loaded_die, 0.1)]
+    transition_casino = [[0.95, 0.05], [0.1, 0.9]]
+    path = "1110000"
+    path_proba = path_probability_computation_correct(casino_dice, path, transition_casino)
+    print(path_proba)
+
+    observation = "3661634"
+    observation_proba = observation_probability_computation_given_path_correct(casino_dice, observation, path)
+    print(observation_proba)
 
 
 if __name__ == "__main__":
