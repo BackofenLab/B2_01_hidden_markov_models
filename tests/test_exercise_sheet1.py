623c753a21d13a95c7069562fa1ef631c097ac42
RickGelhausen <rick.gelhausen@gmail.com>
2022-04-25 13:52:13 +0200

    removed everything but the programming task

--- tests/test_exercise_sheet1.py
+++ tests/test_exercise_sheet1.py
@@ -11,60 +11,6 @@ N_TESTS = 10
 LOAD_PROBABILITIES = [1 / 10, 1 / 10, 1 / 10, 2 / 10, 2 / 10, 3 / 10]
 
 
-def test_exercise_1a():
-    """
-    For details check README spoilers.
-    """
-    response = exercise_1a()
-    solution = 0.98 * (1 / 6) + 0.02 * 0.5
-    assert isclose(response, solution, abs_tol=1e-3)
-
-
-def test_exercise_1b():
-    """
-    For details check README spoilers.
-    """
-    response = exercise_1b()
-    solution = (0.5 ** 3 * 0.02) / (0.5 ** 3 * 0.02 + (1 / 6) ** 3 * 0.98)
-    assert isclose(response, solution, abs_tol=1e-3)
-
-
-def test_exercise_1c():
-    """
-    For details check README spoilers.
-    """
-    response = exercise_1c()
-    solution = 6
-    assert isclose(response, solution, abs_tol=1e-3)
-
-
-def test_exercise_2a():
-    """
-    For details check README.
-    """
-    response = [int(x) for x in exercise_2a()]
-    solution = [2, 6, 4, 5, 1, 1, 5]
-    assert response == solution
-
-
-def test_exercise_2b():
-    """
-    For details check README spoilers.
-    """
-    response = exercise_2b()
-    solution = [1.34 * 10 ** (-7), 2.36 * 10 ** (-6)]
-    assert response == solution
-
-
-def test_exercise_2c():
-    """
-    For details check README spoilers.
-    """
-    response = int(exercise_2c())
-    solution = 16
-    assert response == solution
-
-
 def test_exercise_3a():
     """
     For details check README.
